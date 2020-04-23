84cf38eb271b92f78d6ca01369dc900914e05259
Gavin Sadler <gavinsadler01@gmail.com>
2020-04-23 00:45:38 -0400

    Apparentyl I don't know the difference between a panel and a frame 😬

--- gameInfoPanel.py
+++ gameInfoPanel.py
@@ -2,7 +2,7 @@
 import wx
 
 
-class GameInfoFrame(wx.Frame):
+class GameInfoFrame(wx.Panel):
     """
     A class representing a frame which shows the players score, a timer, number of bombs and flags,
     and other general game info, also having basic functionality to restart the game and access the settings menu
--- minefieldPanel.py
+++ minefieldPanel.py
@@ -2,7 +2,7 @@
 import wx
 
 
-class MinefieldFrame(wx.Frame):
+class MinefieldFrame(wx.Panel):
     """
     A class representing a frame of buttons where the minefield for the minesweeper program is located
     This class handles all of the button presses and interactions with the minesweeper library
